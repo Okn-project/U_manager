ccd508caf3d8bdcae183913a0cc776dbfb5c523f
Roman <Romanklacko5138@gmail.com>
2026-04-20 16:33:32 +0300

    folders structure updated

--- src/views/widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .layers_names_label import LayersNamesLabel
-from .layers_names_label_frame import LayersNamesLabelFrame
-
-__all__ = [
-    'LayersNamesLabel',
-    'LayersNamesLabelFrame',
-]
